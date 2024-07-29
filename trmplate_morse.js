21b6b6687d288dcdb180166d15c76d13b5b28c7d
Tommyisready <perrot.thomas@live.fr>
2024-07-29 13:44:09 +0200

    exo amélioré

--- trmplate_morse.js
+++ trmplate_morse.js
@@ -1,5 +1,3 @@
-// EXO MORSE
-// ANNEXES
 const latinToMorse = {
   A: ".-",
   B: "-...",
@@ -28,6 +26,36 @@ const latinToMorse = {
   Y: "-.--",
   Z: "--..",
 };
+
+// Etape 1
+function get_latin_character_list(text) {
+  return text.split("");
+}
+
+let result = get_latin_character_list("Hello World");
+// console.log(result);
+
+// Etape 2
+
+function translate_latin_character(character) {
+  return latinToMorse[character.toUpperCase()];
+}
+
+// console.log(translate_latin_character("A"));
+
+// Etape 3
+
+function encode(text) {
+  let character_list = get_latin_character_list(text);
+  tab_result = character_list.map(translate_latin_character);
+  return tab_result;
+}
+
+let result1 = encode("Hello World");
+
+// console.log(result1);
+
+// Etape 4
 
 const morseToLatin = {
   "-": "T",
@@ -58,73 +86,130 @@ const morseToLatin = {
   "....": "H",
 };
 
-// ETAPE 1
-// Fonction getLatinCharacterList va prendre en paramètre du texte et retourner un tableau contenant chaque caractère.
-function getLatinCharacterList(text) {
-  return text.split("");
+function get_morse_character_list(text) {
+  return text.split(" ");
+}
+let result2 = get_morse_character_list(".- .-.. --- / .-- --- .-. .-.. -..");
+// console.log(result2);
+
+function translate_morse_character(morse) {
+  return morseToLatin[morse];
 }
 
-let result = getLatinCharacterList("Hello World");
-console.log(result);
+// console.log(translate_morse_character(".-"));
 
-// ETAPE 2
-// Fonction translateLatinCharacter qui prend un paramètre un caractère et renvoie sa correspondance en morse.
-function translateLatinCharacter0(character) {
-  return latinToMorse[character];
+function decode(morse_text) {
+  let words = morse_text.split(" / ");
+  let decode_words = words.map((word) => {
+    let characters = get_morse_character_list(word);
+    let tab_result1 = characters.map(translate_morse_character);
+    return tab_result1.join("");
+  });
+  return decode_words.join(" ");
 }
 
-// Version qui fonctionne aussi avec les minuscules - En convertissant le caractère en majuscule
-function translateLatinCharacter(character) {
-  let charMaj = character.toUpperCase();
-  return latinToMorse[charMaj];
-}
-
-let result2 = translateLatinCharacter("a");
-console.log(result2);
-
-// ETAPE 3
-// Fonction encode qui prend en paramètre du texte et qui va utiliser la fonction de l’étape 1, pour chaque caractère, appliquer la fonction de l’étape 2 et ainsi récupérer son équivalent morse.
-function encode(text) {
-  let characterList = getLatinCharacterList(text);
-  // console.log(characterList)
-  let tabResult = [];
-  for (i = 0; i < characterList.length; i++) {
-    // recuperer le caractere courant - a la position i dans characterList
-    let c = characterList[i];
-    // Le traduire et le stocker dans une variable
-    let cTranslated = translateLatinCharacter(c);
-    // L'ajouter au tableau résultat
-    tabResult.push(cTranslated);
-  }
-  return tabResult;
-}
-
-// Version avec map, qui crée un nouveau tableau avec les résultats de l'appel
-// d'une fonction fournie sur chaque élément du tableau appelant.
-function encodeMap(text) {
-  let characterList = getLatinCharacterList(text);
-  let tabResult = characterList.map(translateLatinCharacter);
-  return tabResult;
-}
-
-let result3 = encode("Hello World");
+let result3 = decode(".... . .-.. .-.. --- / .-- --- .-. .-.. -..");
 console.log(result3);
 
-// ETAPE 4
-// 4.1. Modifier la fonction encode pour qu'elle prenne en compte les espaces
-function encodeNextGen(text) {
-  let characterList = getLatinCharacterList(text);
-  let tabResult = [];
-  for (i = 0; i < characterList.length; i++) {
-    let c = characterList[i];
-    let cTranslated = "";
-    // pas besoin d'accolade si une seule instruction dans un if/else
-    if (c == " ") cTranslated = "/";
-    else cTranslated = translateLatinCharacter(c);
-    tabResult.push(cTranslated);
-  }
-  return tabResult;
-}
+// code avec le html mais qui reste en commentaire
 
-let result4 = encodeNextGen("Hello World");
-console.log(result4);
+// const latinToMorse = {
+//   A: ".-",
+//   B: "-...",
+//   C: "-.-.",
+//   D: "-..",
+//   E: ".",
+//   F: "..-.",
+//   G: "--.",
+//   H: "....",
+//   I: "..",
+//   J: ".---",
+//   K: "-.-",
+//   L: ".-..",
+//   M: "--",
+//   N: "-.",
+//   O: "---",
+//   P: ".--.",
+//   Q: "--.-",
+//   R: ".-.",
+//   S: "...",
+//   T: "-",
+//   U: "..-",
+//   V: "...-",
+//   W: ".--",
+//   X: "-..-",
+//   Y: "-.--",
+//   Z: "--..",
+// };
+
+// function get_latin_character_list(text) {
+//   return text.split("");
+// }
+
+// function translate_latin_character(character) {
+//   return latinToMorse[character.toUpperCase()] || "";
+// }
+
+// function encode(text) {
+//   let character_list = get_latin_character_list(text);
+//   return character_list.map(translate_latin_character).join(" ");
+// }
+
+// const morseToLatin = {
+//   "-": "T",
+//   "--": "M",
+//   "---": "O",
+//   "--.": "G",
+//   "--.-": "Q",
+//   "--..": "Z",
+//   "-.": "N",
+//   "-.-": "K",
+//   "-.--": "Y",
+//   "-.-.": "C",
+//   "-..": "D",
+//   "-..-": "X",
+//   "-...": "B",
+//   ".": "E",
+//   ".-": "A",
+//   ".--": "W",
+//   ".---": "J",
+//   ".--.": "P",
+//   ".-.": "R",
+//   ".-..": "L",
+//   "..": "I",
+//   "..-": "U",
+//   "..-.": "F",
+//   "...": "S",
+//   "...-": "V",
+//   "....": "H",
+// };
+
+// function get_morse_character_list(text) {
+//   return text.split(" ");
+// }
+
+// function translate_morse_character(morse) {
+//   return morseToLatin[morse] || "";
+// }
+
+// function decode(morse_text) {
+//   let words = morse_text.split(" / ");
+//   return words
+//     .map((word) => {
+//       let characters = get_morse_character_list(word);
+//       return characters.map(translate_morse_character).join("");
+//     })
+//     .join(" ");
+// }
+
+// function translateToMorse() {
+//   let text = document.getElementById("text-input").value;
+//   let result = encode(text);
+//   document.getElementById("result").value = result;
+// }
+
+// function translateToText() {
+//   let morse = document.getElementById("morse-input").value;
+//   let result = decode(morse);
+//   document.getElementById("result").value = result;
+// }
